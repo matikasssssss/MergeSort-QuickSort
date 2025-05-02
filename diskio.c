0b899f933a89226077b9c964fe4e9152c58c7dc5
matikasssssss <matias.farias@ug.uchile.cl>
2025-05-01 21:11:21 -0400

    Primer avance tarea; todo: Quicksort y mergesort

--- diskio.c
+++ diskio.c
@@ -1,19 +1,19 @@
 #include "diskio.h"
 #include <stdlib.h>
 
-size_t contador_lecturas = 0;
-size_t contador_escrituras = 0;
+size_t cont_lecturas = 0;
+size_t cont_escrituras = 0;
 
 void leer_bloque(FILE *f, int64_t *buffer, size_t bloque)
 {
     fseek(f, bloque * BLOCK_SIZE, SEEK_SET);
     fread(buffer, ELEMENT_SIZE, ELEMENTS_PER_BLOCK, f);
-    contador_lecturas++;
+    cont_lecturas++;
 }
 
 void escribir_bloque(FILE *f, int64_t *buffer, size_t bloque)
 {
     fseek(f, bloque * BLOCK_SIZE, SEEK_SET);
     fwrite(buffer, ELEMENT_SIZE, ELEMENTS_PER_BLOCK, f);
-    contador_escrituras++;
+    cont_escrituras++;
 }
--- diskio.h
+++ diskio.h
@@ -11,7 +11,7 @@
 void leer_bloque(FILE *f, int64_t *buffer, size_t bloque);
 void escribir_bloque(FILE *f, int64_t *buffer, size_t bloque);
 
-extern size_t contador_lecturas;
-extern size_t contador_escrituras;
+extern size_t cont_lecturas;
+extern size_t cont_escrituras;
 
 #endif
