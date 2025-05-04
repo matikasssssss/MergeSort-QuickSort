41c5a50038b1d12a4a2cc5d7ec8590aba8d72922
matikasssssss <matias.farias@ug.uchile.cl>
2025-05-04 19:15:23 -0400

    mergesort

--- mergesort.c
+++ mergesort.c
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 void intercambiar_elementos(int64_t *arr, size_t i, size_t j)
 {
     int64_t tmp = arr[i];
@@ -86,19 +85,6 @@ void mezclar_archivos(char **input_names, size_t count, const char *output_name)
 
         leer_bloque(entradas[i].f, entradas[i].buffer, entradas[i].bloque_actual++);
         entradas[i].elementos_validos = ELEMENTS_PER_BLOCK;
-void mezclar_archivos(char **input_names, size_t count, const char *output_name)
-{
-    FILE **inputs = malloc(sizeof(FILE *) * count);
-    int64_t *buffer = malloc(sizeof(int64_t) * count);
-    int activo[count];
-    size_t pos[count];
-
-    for (size_t i = 0; i < count; i++)
-    {
-        inputs[i] = fopen(input_names[i], "rb");
-        fread(&buffer[i], sizeof(int64_t), 1, inputs[i]);
-        activo[i] = 1;
-        pos[i] = 1;
     }
 
     FILE *out = fopen(output_name, "wb");
@@ -114,11 +100,6 @@ void mezclar_archivos(char **input_names, size_t count, const char *output_name)
             if (entradas[i].activo && entradas[i].pos < entradas[i].elementos_validos)
             {
                 if (min_idx == -1 || entradas[i].buffer[entradas[i].pos] < entradas[min_idx].buffer[entradas[min_idx].pos])
-        for (size_t i = 0; i < count; i++)
-        {
-            if (activo[i])
-            {
-                if (min_idx == -1 || buffer[i] < buffer[min_idx])
                 {
                     min_idx = i;
                 }
@@ -158,9 +139,6 @@ void mezclar_archivos(char **input_names, size_t count, const char *output_name)
             out_buffer[i] = 0;
         }
         escribir_bloque(out, out_buffer, ftell(out) / BLOCK_SIZE);
-    for (size_t i = 0; i < count; i++)
-    {
-        fclose(inputs[i]);
     }
 
     for (size_t i = 0; i < count; i++)
